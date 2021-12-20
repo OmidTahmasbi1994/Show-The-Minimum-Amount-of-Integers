d2d838557a22ae7b4ecb7e1cfaf1fe1c0867ca95
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-21 01:35:03 +0330

    Add files via upload

---
+++ Show The Minimum of Integers 1.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+
+{
+	int a,b,c ;
+	cout<<"Enter Three Integers :  "<<endl;
+	cout<<"a : ";
+	cin>>a;
+	cout<<"Enter b: ";
+	cin>>b;
+	cout<<"Enter c: ";
+	cin>>c;
+	
+	int minimum = a;
+	
+	if (b<minimum) minimum=b ;
+	if (c<minimum) minimum=c ;
+	
+	cout<<"Their Minimum Is: "<<minimum<<endl;
+	
+	getch();
+	
+	
+	
+}
